03ec292a2ae5d72e2cdc09f73b2c6270041d3dd6
blurbeast <leumasre@gmail.com>
2025-03-07 22:35:03 +0100

    app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlockchainModule } from './blockchain/blockchain.module';
+import { PlayerModule } from './player/player.module';
+import { GameModule } from './game/game.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [process.env.DB_ENTITIES as string],
+      synchronize: true,
+    }),
+    BlockchainModule,
+    PlayerModule,
+    GameModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
