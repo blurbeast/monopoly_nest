d0c56e6a452d2bc6b45ff25763d2d50d6c9efbf6
blurbeast <leumasre@gmail.com>
2025-03-08 20:28:37 +0100

    resolving imports

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as dotenv from 'dotenv';
+import * as PlayerAbi from './abis/PlayerAbi.json';
+dotenv.config();
+
 
 @Injectable()
-export class BlockchainService {}
+export class BlockchainService {
+
+    private readonly playerContractAddress: ethers.Contract;
+    private readonly entryPointContractAddress: ethers.Contract;
+    private readonly defaultWallet: ethers.Wallet;
+    private readonly provider: ethers.JsonRpcProvider;
+
+    constructor() {
+        if (!process.env.WALLET_KEY ||
+            !process.env.PROVIDER_URL) {
+            throw new Error("could not read from the environment variables");
+        }
+
+        this.provider = new ethers.JsonRpcProvider(process.env.PROVIDER_URL as string);
+        this.defaultWallet = new ethers.Wallet(
+            process.env.WALLET_KEY as string,
+            this.provider
+        );
+
+        this.playerContractAddress = new ethers.Contract(process.env.PLAYER_CONTRACT_ADDRESS as string, PlayerAbi, this.defaultWallet);
+    }
+}
