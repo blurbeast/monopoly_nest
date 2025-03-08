feae7748e991bc4c92b97e019ea729168a9dd2ed
blurbeast <leumasre@gmail.com>
2025-03-08 16:34:56 +0100

    game entity fields

--- src/game/game.entity.ts
+++ src/game/game.entity.ts
@@ -1,7 +1,26 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Player } from 'src/player/player.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Game {
-  @PrimaryGeneratedColumn()
-  id: string;
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ unique: true, type: 'char', length: 4 })
+    gameRoomId: string;
+
+    @Column({ name: 'game_players' })
+    players: Player[]
+
+    @Column({ unique: true, length: 42, type: 'varchar' })
+    bankContractAddress: string;
+
+    @Column({ type: 'timestamp', name: 'created_time', })
+    createAt: Date;
+
+    @Column()
+    currentTurn: string;
+
+    @Column({ type: 'timestamp', name: 'ended_time', })
+    endedAt: Date;
 }
